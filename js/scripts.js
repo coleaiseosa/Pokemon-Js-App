1d766cf6608b4ec963c91084b00d994d8abbd360
Marcella Cole <coleaiseosa@gmail.com>
2022-09-05 16:27:13 +0100

    Wrapped the array in IIFE and refactored the codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,44 @@
-let pokemonList = [ // creating an array of objects
-  {
-    name : 'Bulbasaur',
-    height : 0.7,
-    types : ['grass', 'poison']
-  },
-  {
-    name : 'Charizard',
-    height : 1.7,
-    types : ['fire', 'flying']
-  },
-  {
-    name : 'Butterfree',
-    height : 1.1,
-    types : ['bug', 'flying']
-  },
-  {
-    name : 'Kakuna',
-    height : 0.6,
-    types : ['bug', 'poison']
+let pokemonRepository = (function() {
+  let pokemonList = [ // creating an array of objects
+    {
+      name : 'Bulbasaur',
+      height : 0.7,
+      types : ['grass', 'poison']
+    },
+    {
+      name : 'Charizard',
+      height : 1.7,
+      types : ['fire', 'flying']
+    },
+    {
+      name : 'Butterfree',
+      height : 1.1,
+      types : ['bug', 'flying']
+    },
+    {
+      name : 'Kakuna',
+      height : 0.6,
+      types : ['bug', 'poison']
+    }
+  ];
+
+  function getAll() {
+    return pokemonList;
+  };
+
+  function add(item) {
+    return pokemonList.push(item) ? {} : '';
+    // pokemonList.push(item)
   }
-]
+
+  return {
+    getAll : getAll,
+    add : add
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: 'Ivysaur', height: 1, types : ['grass', 'poison']});
 
 // for (let i = 0; i < pokemonList.length; i++ ) { to loop through each pokemon character
 //   if  (pokemonList[i].height > 1.5) {
@@ -29,7 +48,9 @@ let pokemonList = [ // creating an array of objects
 //   }
 // }
 
-pokemonList.forEach(function(pokemon) {
-  console.log(pokemon.name + ' height: ' + pokemon.height + ' ' + pokemon.types );
-    document.write('<p>' + pokemon.name + ' height: '  + pokemon.height + ' ' + pokemon.types + '</p>')
-})
+let pokemonList = pokemonRepository.getAll();
+
+ pokemonList.forEach(function(pokemon) {
+    console.log(pokemon.name + ' height: ' + pokemon.height + ' ' + pokemon.types );
+    document.write('<p>' + pokemon.name + ' height: '  + pokemon.height + ' ' + pokemon.types + '</p>');
+ })
