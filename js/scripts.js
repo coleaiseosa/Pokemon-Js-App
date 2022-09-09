98c00398cff9018dab8a443d401c757e0c0f9059
Marcella Cole <coleaiseosa@gmail.com>
2022-09-09 13:46:37 +0100

    fetched data from pokemon Api

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,98 @@
 let pokemonRepository = (function() {
-  let pokemonList = [ // creating an array of objects
-    {
-      name : 'Bulbasaur',
-      height : 0.7,
-      types : ['grass', 'poison']
-    },
-    {
-      name : 'Charizard',
-      height : 1.7,
-      types : ['fire', 'flying']
-    },
-    {
-      name : 'Butterfree',
-      height : 1.1,
-      types : ['bug', 'flying']
-    },
-    {
-      name : 'Kakuna',
-      height : 0.6,
-      types : ['bug', 'poison']
-    }
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function getAll() {
     return pokemonList;
   };
 
-  function add(item) {
-    return pokemonList.push(item) ? {} : '';
-    // pokemonList.push(item)
+  function add(pokemon) {
+    return pokemonList.push(pokemon) ? {} : '';
+     //pokemonList.push(item)
   };
+
+//  another way to add the add function
+//   function add(pokemon) {
+//   if (typeof pokemon === "object" && "name" in pokemon) {
+//     pokemonList.push(pokemon);
+//   } else {
+//     console.log('pokemon is not correct')
+//   }
+// }
 
   function addListItem(pokemon) {
     let allPokemon = document.querySelector('.pokemon-list');
-
     let listItem = document.createElement('li');
-
     let button = document.createElement('button');
-    button.innerText = pokemon.name; //to enable the for each loob return each name of the pokemon characters do not put the pokemon.name in quotation marks
-
-     //listens to clicks on the pokemon buttons created and returns the information of the button
-    button.addEventListener('click', function(event) { // the event in the function can be any word but its best to stick with event
-      showDetails(pokemon); //event handler
-    })
-
+    //to enable the for each loob return each name of the pokemon characters do not put the pokemon.name in quotation marks
+    button.innerText = pokemon.name;
     button.classList.add('button');
     listItem.appendChild(button);
     allPokemon.appendChild(listItem);
-
+    //listens to clicks on the pokemon buttons created and returns the information of the button
+    button.addEventListener('click', function(event) { // the event in the function can be any word but its best to stick with event
+      showDetails(pokemon); //event handler
+    });
   };
 
-  function showDetails(pokemon) {
-    console.log(pokemon);
-  }
+function showLoadingMessage() {
+  console.log('Please wait a few minutes');
+}
 
-  // function newButtons(pokemon) {
-  //     let button = document.createElement('button');
-  //     showDetails(pokemon)
-  // }
+  //load list
+  function loadList() {
+    showLoadingMessage();
+      return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  };
+
+//load details
+function loadDetails(item) {
+  showLoadingMessage();
+  let url = item.detailsUrl;
+  return fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (details) {
+    // Now we add the details to the item
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = details.types;
+  }).catch(function (e) {
+    console.error(e);
+  });
+};
+
+function showDetails(pokemon) {
+  loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+  });
+}
 
   return {
     getAll : getAll,
     add : add,
     addListItem : addListItem,
-
+    loadList : loadList,
+    loadDetails : loadDetails,
+    showDetails : showDetails,
+    showLoadingMessage : showLoadingMessage
   };
 })();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({name: 'Ivysaur', height: 1, types : ['grass', 'poison']});
+//console.log(pokemonRepository.getAll());
+//pokemonRepository.add({name: 'Ivysaur', height: 1, types : ['grass', 'poison']});
 
 // for (let i = 0; i < pokemonList.length; i++ ) { to loop through each pokemon character
 //   if  (pokemonList[i].height > 1.5) {
@@ -78,6 +102,9 @@ pokemonRepository.add({name: 'Ivysaur', height: 1, types : ['grass', 'poison']})
 //   }
 // }
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
