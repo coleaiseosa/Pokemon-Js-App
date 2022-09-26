531320365ebd0cb0a219d38210f6feb3cb4fc3e0
Marcella Cole <coleaiseosa@gmail.com>
2022-09-26 14:53:00 +0100

    Changed variable declaration

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-let pokemonRepository = (function() {
-  let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+const pokemonRepository = (function() {
+  const pokemonList = [];
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=51';
 
   function getAll() {
     return pokemonList;
   };
 
   function add(pokemon) {
-  if (typeof pokemon === "object" && "name" in pokemon && "detailsUrl" in pokemon) {
+  if (typeof pokemon === 'object' && 'name' in pokemon && 'detailsUrl' in pokemon) {
     pokemonList.push(pokemon);
   } else {
     console.log('pokemon is not correct')
@@ -15,10 +15,10 @@ let pokemonRepository = (function() {
 }
 
   function addListItem(pokemon) {
-    let allPokemon = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
+    const allPokemon = document.querySelector('.pokemon-list');
+    const listItem = document.createElement('li');
     //listItem.classList.add('list-group-item')
-    let button = document.createElement('button');
+    const button = document.createElement('button');
     //to enable the for each loop return each name of the pokemon characters do not put the pokemon.name in quotation marks
     button.innerText = pokemon.name;
     button.classList.add('button');
@@ -39,7 +39,7 @@ let pokemonRepository = (function() {
       return response.json();
     }).then(function (json) {
       json.results.forEach(function (item) {
-        let pokemon = {
+        const pokemon = {
           name: item.name,
           detailsUrl: item.url
         };
@@ -53,7 +53,7 @@ let pokemonRepository = (function() {
 
 //load details
 function loadDetails(item) {
-  let url = item.detailsUrl;
+  const url = item.detailsUrl;
   return fetch(url).then(function (response) {
     return response.json();
   }).then(function (details) {
@@ -75,37 +75,14 @@ function showDetails(pokemon) {
 }
 
 //modal
-function showModal(pokemon) {
-  let modalBody = $('.modal-body');
-  let modalTitle = $('.modal-title');
-  let modalHeader = $('.modal-header');
-
-  // to clear existing modal contents
-  modalTitle.empty();
-  modalBody.empty();
-
-  //to create elements in modal content
-  let nameElement = $('<h1>' + pokemon.name + '</h1>');
-  let imageElement = $('<img class="modal-img">');
-  imageElement.attr('src', pokemon.imageUrl);
-  let heightElement = $('<p>' + 'Height: ' + pokemon.height + '</p>');
-  let weightElement = $('<p>' + 'Weight: ' + pokemon.weight + '</p>');
-  let typesElement = $('<p>' + 'Types: ' + pokemon.types + '</p>');
-
-  modalTitle.append(nameElement);
-  modalBody.append(imageElement);
-  modalBody.append(heightElement);
-  modalBody.append(weightElement);
-  modalBody.append(typesElement);
-}
 
   return {
-    getAll : getAll,
-    add : add,
-    addListItem : addListItem,
-    loadList : loadList,
-    loadDetails : loadDetails,
-    showDetails : showDetails
+    getAll,
+    add,
+    addListItem,
+    loadList,
+    loadDetails,
+    showDetails
   };
 })();
 
